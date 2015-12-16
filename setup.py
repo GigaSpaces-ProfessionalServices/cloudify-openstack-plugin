9ef9bebe8463d55ee3d1ec8043055118c306f5b0
Isaac Shabtay <isaac@shabtay.com>
2015-12-16 10:36:44 -0800

    Updates

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-openstack-plugin',
-    version='1.3sgas',
+    version='1.3rc1',
     author='isaac-s',
     author_email='isaac@gigaspaces.com',
     packages=[
